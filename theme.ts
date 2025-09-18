97d94d222dd71c76c1a34237f5be46856a586457
gjermundmyrvang <myrvang.gjermund@gmail.com>
2025-09-18 16:53:21 +0200

    create theme

---
+++ theme.ts
@@ -0,0 +1,22 @@
+import { MD3LightTheme as DefaultTheme } from "react-native-paper";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#006466",
+    onPrimary: "#ffffff",
+    secondary: "#0b525b",
+    onSecondary: "#ffffff",
+    tertiary: "#4d194d",
+    onTertiary: "#ffffff",
+    background: "#212f45",
+    onBackground: "#ffffff",
+    surface: "#272640",
+    onSurface: "#ffffff",
+    surfaceVariant: "#312244",
+    outline: "#3e1f47",
+  },
+};
+
+export default theme;
