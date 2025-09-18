185b2ca99ab97ba1c988bc5156a5a5d80aef2678
gjermundmyrvang <myrvang.gjermund@gmail.com>
2025-09-18 16:53:43 +0200

    wrap app inside theme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { PaperProvider } from "react-native-paper";
 import Homescreen from "./screens/Homescreen";
+import theme from "./theme";
 
 export default function App() {
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <Homescreen />
     </PaperProvider>
   );
